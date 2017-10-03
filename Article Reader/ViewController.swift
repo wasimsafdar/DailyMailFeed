b2eb5b85655d0e0adee996532f76b34ea83e3806
wasimsafdar <engr.safdarwasim@gmail.com>
2017-10-03 10:42:26 +0200

    Initial Commit

---
+++ Article Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Article Reader
+//
+//  Created by Eng Waseem on 03/10/2017.
+//  Copyright © 2017 Humma Embedded Consultancy and Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
